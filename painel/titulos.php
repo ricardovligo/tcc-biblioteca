96fc4ccd16f22cc0e8349dab145abc8f692a792c
Ricardo <ricardo.vligo@gmail.com>
2021-07-28 21:52:29 -0300

    Update titulos.php

--- painel/titulos.php
+++ painel/titulos.php
@@ -1,9 +1,21 @@
 <?php include "topo.php"; ?>
 
 <div id="Corpo">
+
     <div class="Conteudo">
+
         <div class="BotaoAdicionar">Adicionar</div>
+
+        <div class="Tabela">
+            <!-- <div>Título</div>
+            <div>Categoria</div>
+            <div>Emprestado</div>
+            <div>Disponível</div>
+            <div>Editar</div>
+            <div>Excluir</div> -->
+        </div>
     </div>
+
 </div>
 
 <?php include "rodape.php"; ?>
