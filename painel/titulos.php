971c255c623f6cf684cad0ad8577205109652ea3
Ricardo <ricardo.vligo@gmail.com>
2021-07-28 22:48:14 -0300

    Update titulos.php

--- painel/titulos.php
+++ painel/titulos.php
@@ -3,7 +3,7 @@
 <div id="Corpo">
 
     <div class="Conteudo">
-        <div style="width:100%; height:100px;"><div class="BotaoAdicionar">Adicionar</div></div>
+        <div style="width:100%; height:140px;"><div class="BotaoAdicionar">Adicionar</div></div>
 
         <div class="Tabela">
             <div class="TabelaLinha" style="background-color:#8a8a8a;">
@@ -16,12 +16,12 @@
             </div>
             <?php for ($i = 0; $i < 5; $i++) { ?>
             <div class="TabelaLinha">
-                <div class="TabelaLinhaColuna" style="width:60%">Título</div>
-                <div class="TabelaLinhaColuna" style="width:14%">Categoria</div>
-                <div class="TabelaLinhaColuna" style="width:8%">Emprestado</div>
-                <div class="TabelaLinhaColuna" style="width:8%">Disponível</div>
-                <div class="TabelaLinhaColuna" style="width:5%">Editar</div>
-                <div class="TabelaLinhaColuna" style="width:5%">Excluir</div>
+                <div class="TabelaLinhaColuna" style="width:60%">Harry Potter</div>
+                <div class="TabelaLinhaColuna" style="width:14%">Aventura</div>
+                <div class="TabelaLinhaColuna" style="width:8%">1</div>
+                <div class="TabelaLinhaColuna" style="width:8%">3</div>
+                <div class="TabelaLinhaColuna" style="width:5%">-</div>
+                <div class="TabelaLinhaColuna" style="width:5%">-</div>
             </div>
             <?php } ?>
         </div>
