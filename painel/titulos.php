d19cfab65d7368a2e554c5a18bf031d183b3fee2
Ricardo <ricardo.vligo@gmail.com>
2021-07-28 22:37:50 -0300

    Update titulos.php

--- painel/titulos.php
+++ painel/titulos.php
@@ -7,18 +7,18 @@
 
         <div class="Tabela">
             <div class="TabelaLinha">
-                <div class="TabelaLinhaColuna" style="width:70%">Título</div>
-                <div class="TabelaLinhaColuna" style="width:10%">Categoria</div>
-                <div class="TabelaLinhaColuna" style="width:5%">Emprestado</div>
-                <div class="TabelaLinhaColuna" style="width:5%">Disponível</div>
+                <div class="TabelaLinhaColuna" style="width:60%">Título</div>
+                <div class="TabelaLinhaColuna" style="width:16%">Categoria</div>
+                <div class="TabelaLinhaColuna" style="width:8%">Emprestado</div>
+                <div class="TabelaLinhaColuna" style="width:8%">Disponível</div>
                 <div class="TabelaLinhaColuna" style="width:5%">Editar</div>
                 <div class="TabelaLinhaColuna" style="width:5%">Excluir</div>
             </div>
             <div class="TabelaLinha">
-                <div class="TabelaLinhaColuna" style="width:70%">Título</div>
-                <div class="TabelaLinhaColuna" style="width:10%">Categoria</div>
-                <div class="TabelaLinhaColuna" style="width:5%">Emprestado</div>
-                <div class="TabelaLinhaColuna" style="width:5%">Disponível</div>
+                <div class="TabelaLinhaColuna" style="width:60%">Título</div>
+                <div class="TabelaLinhaColuna" style="width:16%">Categoria</div>
+                <div class="TabelaLinhaColuna" style="width:8%">Emprestado</div>
+                <div class="TabelaLinhaColuna" style="width:8%">Disponível</div>
                 <div class="TabelaLinhaColuna" style="width:5%">Editar</div>
                 <div class="TabelaLinhaColuna" style="width:5%">Excluir</div>
             </div>
