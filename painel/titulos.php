2eb4431ee9e9f8270085ac1bb0eb6a6427ea0718
Ricardo <ricardo.vligo@gmail.com>
2021-07-28 22:38:32 -0300

    Update titulos.php

--- painel/titulos.php
+++ painel/titulos.php
@@ -6,7 +6,7 @@
         <div style="width:100%; height:100px;"><div class="BotaoAdicionar">Adicionar</div></div>
 
         <div class="Tabela">
-            <div class="TabelaLinha">
+            <div class="TabelaLinha" style="background-color:#8a8a8a;">
                 <div class="TabelaLinhaColuna" style="width:60%">Título</div>
                 <div class="TabelaLinhaColuna" style="width:16%">Categoria</div>
                 <div class="TabelaLinhaColuna" style="width:8%">Emprestado</div>
