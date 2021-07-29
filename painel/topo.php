a6b8d4ca96fc9830133e53672a03065bba5ce922
Ricardo <ricardo.vligo@gmail.com>
2021-07-28 21:14:56 -0300

    Create topo.php

---
+++ painel/topo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="js_css/estilo.css" rel="stylesheet">
+    <title>Biblioteca - Painel</title>
+</head>
+<body>
+<div id="Topo">
+    <div class="BotoesTopo">
+        <div class="BotoesTopoOpcoes">Títulos</div>
+        <div class="BotoesTopoOpcoes" style="margin-left:50px;">Empréstimos</div>
+    </div>
+    <div class="BotaoSairTopo BotoesTopoOpcoes">Sair</div>
+</div>
